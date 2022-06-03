f913fcfbeeabd2777ef576123f4bca38b265da2a
Your Name <you@example.com>
2022-06-03 18:46:26 +0200

    bootstrap on progress

--- rpi/pwa/index.js
+++ rpi/pwa/index.js
@@ -5,9 +5,14 @@ const be = require("./backend/backend.js")
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public/views'))
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
 // ROUTES
 
