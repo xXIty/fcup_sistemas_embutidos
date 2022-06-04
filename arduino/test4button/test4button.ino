02974ea0d800318b322bbeba05a62da350632cdc
Diogo-Vale <dcvale@outlook.com>
2022-06-04 18:20:50 +0100

    receiving acks and using leds (possibly)

--- arduino/test4button/test4button.ino
+++ arduino/test4button/test4button.ino
@@ -1,12 +1,12 @@
 #define ROWS_COUNT 2
 #define COLS_COUNT 1
-
 #define TIME_DEBOUNCE 15
+#define LED_GREEN 3
+#define LED_RED 2
 
 // Functions
 
 int get_button_pressed();
-
 
 int ROWS[ROWS_COUNT] = {2,3}, COLS[COLS_COUNT] = {11};//,12};
 
@@ -15,7 +15,6 @@ void setup(){
   
   // matrix setup
   for(int R = 0; R < ROWS_COUNT; R++){
-    
     pinMode(ROWS[R],INPUT_PULLUP);
   }
   for(int C = 0; C < COLS_COUNT; C++){
@@ -24,8 +23,10 @@ void setup(){
   }
   
   // led setup
-  pinMode(8,OUTPUT); 
-  digitalWrite(8,LOW);
+  pinMode(2,OUTPUT); 
+  digitalWrite(2,LOW);
+  pinMode(3,OUTPUT); 
+  digitalWrite(3,LOW);
   
   Serial.begin(9600);
   Serial.println(String("Setup done"));
@@ -35,14 +36,47 @@ void setup(){
 
 void loop(){
 
-  
   int square_pos = get_button_pressed();
   if (square_pos != -1)
+    //serial_flush_buffer();
     send_square(square_pos);
-    
+    int ack=get_ack();
+    light_led(ack);
   delay(TIME_DEBOUNCE*10);
-  
 }
+
+/*
+void serial_flush_buffer()
+{
+  while (Serial.read() >= 0)
+   ; // do nothing
+}
+*/
+
+void light_led(int ack){
+  switch(ack){
+    case 0:
+      turn_led_off(LED_RED);
+      turn_led_on(LED_GREEN);
+      break;
+    case 1:
+      turn_led_off(LED_GREEN);
+      turn_led_on(LED_RED);
+      break;
+  }
+}
+
+
+int get_ack(){
+  if(Serial.avaiable()>0){
+    int ack=Serial.read();
+    Serial.println(String("Ack received: "+ack));
+    Serial.flush();
+    return ack;
+  }
+  return -1
+}
+
 
 
 int get_button_pressed() {
@@ -74,10 +108,10 @@ void send_square(int square_pos) {
   Serial.flush();
 }
 
-void turn_led_on() {
-  digitalWrite(8,HIGH);
+void turn_led_on(int pin) {
+  digitalWrite(pin,HIGH);
 }
 
-void turn_led_off() {
-  digitalWrite(8,LOW);
+void turn_led_off(int pin) {
+  digitalWrite(pin,LOW);
 }
